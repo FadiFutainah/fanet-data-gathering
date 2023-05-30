6e032fd19e026b5586bfbd6c708cc34a3b8c25cb
FadiFutainah <fadiftenaft@gmail.com>
2023-05-30 11:24:16 +0300

    edit data transition based on the research paper

--- environment/utils/data_transition.py
+++ environment/utils/data_transition.py
@@ -2,8 +2,10 @@ from environment.devices.device import Device
 
 
 class DataTransition:
-    def __init__(self, source: Device, destination: Device, number_of_packets: int, created_time: int) -> None:
+    def __init__(self, source: Device, destination: Device, number_of_packets: int, created_time: int,
+                 data_size: int) -> None:
         self.source = source
+        self.data_size = data_size
         self.destination = destination
         self.created_time = created_time
         self.number_of_packets = number_of_packets
