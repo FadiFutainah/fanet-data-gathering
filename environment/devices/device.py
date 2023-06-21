4b1541cf56426be44d3f9f6276e7cad464e02a17
FadiFutainah <fadiftenaft@gmail.com>
2023-06-21 13:07:36 +0300

    finish devices module

--- environment/devices/device.py
+++ environment/devices/device.py
@@ -27,17 +27,17 @@ class Device(PhysicalObject):
 
     def move_to_buffer_queue(self, data_size: int) -> None:
         data_packets = self.memory.fetch_data(data_size)
-        self.receive_in_buffer(data_packets)
-
-    def receive_in_buffer(self, data_packets: List[DataPackets]) -> None:
         self.buffer.store_data(data_packets)
 
-    def remove_from_buffer(self, data_size: int) -> List[DataPackets]:
+    def move_from_buffer_queue(self, data_size: int) -> List[DataPackets]:
         return self.buffer.fetch_data(data_size)
 
     def move_to_memory(self, data_size) -> None:
         data_packets = self.buffer.fetch_data(data_size)
         self.memory.store_data(data_packets)
+
+    def move_form_memory(self, data_size: int) -> List[DataPackets]:
+        return self.memory.fetch_data(data_size)
 
     def is_connected_to(self, id: int) -> bool:
         index = self.connected_devices.searchsorted(v=id)
@@ -45,10 +45,8 @@ class Device(PhysicalObject):
 
     def connect_to(self, id: int):
         if self.is_connected_to(id):
-            raise ConnectionAbortedError(f'{self} is already connected to device:{id}')
+            logging.error(f'self{self} is already connected to device {id}')
+            return
         index = self.connected_devices.searchsorted(v=id)
         np.insert(self.connected_devices, index, id)
-        logging.info(f'connection established between {self} and device_id: {id}')
-
-    def step(self) -> None:
-        pass
+        logging.info(f'connection has been established between {self} and device {id}')
--- environment/devices/uav.py
+++ environment/devices/uav.py
@@ -1,5 +1,3 @@
-import logging
-from copy import copy
 from typing import List
 
 import numpy as np
@@ -9,7 +7,6 @@ from dataclasses import dataclass, field
 from environment.networking.connection_protocol import ConnectionProtocol, protocol1
 from environment.networking.data_transition import DataTransition
 from environment.devices.device import Device
-from environment.utils.vector import Vector
 
 
 @dataclass
@@ -20,48 +17,11 @@ class UAV(Device):
     energy: int
     coverage_radius: int
     network_bandwidth: int
-    way_points: List[Vector]
     protocol: ConnectionProtocol = protocol1
     areas_collection_rates: List[int] = field(default=List[int])
-    current_way_point: int = field(init=False, default=-1)
-    slow_down_by_distance_unit: int = field(init=False, default=10)
-    energy_loss_per_step: int = field(init=False, default=10)
-    # data_transitions: Dict[int, List[DataTransition]] = field(init=False)
 
     def __post_init__(self):
         self.uavs_data_transitions = {}
-
-    def move(self) -> None:
-        distance_to_go = copy(self.speed)
-        while distance_to_go > 0:
-            if self.has_reached():
-                self.position = self.way_points[self.current_way_point]
-                logging.warning(f'{self} has reached the last way point')
-                break
-            next_way_point = self.way_points[self.current_way_point + 1]
-            distance = self.position.distance_from(next_way_point)
-            if distance_to_go < distance:
-                factor = distance_to_go / distance
-                x = (next_way_point.x - self.position.x) * factor
-                y = (next_way_point.y - self.position.y) * factor
-                z = (next_way_point.z - self.position.z) * factor
-                self.position = self.position + Vector(x, y, z)
-                break
-            if distance_to_go == distance:
-                self.current_way_point += 1
-                self.position = self.way_points[self.current_way_point]
-                break
-            self.current_way_point += 1
-            distance_to_go -= distance
-
-    def step(self) -> None:
-        if self.energy <= 0:
-            logging.error(f'{self} has no more energy')
-            return
-        self.energy -= self.energy_loss_per_step
-
-    def has_reached(self) -> bool:
-        return self.current_way_point == len(self.way_points) - 1
 
     def in_range(self, other: Device) -> bool:
         return self.position.distance_from(other.position) <= self.coverage_radius
